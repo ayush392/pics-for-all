af3a342184bbd6ce236e56a6aa7628c4b51fb521
ayush392 <ayushgoyal.0004@gmail.com>
2023-10-08 14:28:02 +0530

    refactor posts, postsController code

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,28 +1,154 @@
-const router = require("express").Router();
-const multer = require('multer');
-const UserToUploadMapping = require('../models/UserToUploadMapping')
-const path = require('path');
-const uploadFolder = path.join(__dirname, "Videos"); // use a variable to hold the value of upload folder
+const { getImageURL } = require("../config/firebase");
+const Post = require("../models/postModel");
+const { UserDetail } = require("../models/userModel");
+const fbStorage = require("firebase/storage");
+const { storage } = require("../config/firebase");
 
-const storage = multer.diskStorage({
-    destination: uploadFolder, // use it when upload
-    filename: (req, file, cb) => {
-        // nameFile = file.originalname + " "+ Date.now() // --> give "video.mp4 1622180824748"
-        let [filename, extension] = file.originalname.split('.');
-        let nameFile = filename + "-" + Date.now() + "." + extension; // --> give "video-1622181268053.mp4"
-        cb(null, nameFile)
-    }
-})
+// GET
 
-const upload = multer({ storage: storage })
+const allPosts = async (req, res) => {
+  try {
+    const post = await Post.find({}).sort({ date: -1 });
+    res.json(post);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
 
-router.post('/upload', upload.single('video'), async (req, res, next) => {
-    const saveMapping = new UserToUploadMapping({
-        userId: '123',
-        file: req.file,
-    })
+const searchPost = async (req, res) => {
+  const query = req.params.query;
+  try {
+    // { email: new RegExp(`^${emailVariable}$`, 'i') }
+    const post = await Post.find({ tags: new RegExp(`^${query}$`, "i") });
+    // console.log(post, 28);
+    res.json(post);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
 
-    await saveMapping.save()
+const singlePost = async (req, res) => {
+  res.json(res.post);
+};
 
-    res.send("Video uploaded")
-})
+const downloadImage = async (req, res) => {
+  // console.log('96');
+  let imgPath = res.post.image;
+  res.download(`./public/${imgPath}`);
+};
+
+// POST
+const createPost = async (req, res) => {
+  try {
+    const { description, tags, location, username } = req.body;
+    const userDetails = await UserDetail.findOne({ username });
+
+    const tagsArr = String(tags).split(",");
+    tagsArr.forEach((ele, idx) => {
+      tagsArr[idx] = ele.trim();
+    });
+
+    const downloadURL = await getImageURL(req);
+    const newPost = new Post({
+      description: description,
+      tags: tagsArr,
+      location: location,
+      image: downloadURL,
+      user: userDetails,
+    });
+
+    const response = await newPost.save();
+
+    console.log("File successfully uploaded.");
+    res.status(201).json("response");
+  } catch (e) {
+    console.log({ message: e.message });
+    res.status(400).json({ message: e.message });
+  }
+};
+
+//PATCH
+const updatePost = async (req, res) => {
+  const { description, tags, location } = req.body;
+
+  const tagsArr = String(tags).split(",");
+  tagsArr.forEach((ele, idx) => {
+    tagsArr[idx] = ele.trim();
+  });
+
+  if (
+    res.post.description === description &&
+    res.post.tags === tagsArr &&
+    res.post.location === location
+  ) {
+    res.status(200).json({ message: "post updated" });
+    return;
+  }
+
+  res.post.description = description;
+  res.post.tags = tagsArr;
+  res.post.location = location;
+
+  try {
+    const updatePost = await res.post.save();
+    res.status(200).json(updatePost);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+// PUT
+const likePost = async (req, res) => {
+  const { postId, username } = req.body;
+  try {
+    const x = await Post.findByIdAndUpdate(
+      { _id: postId },
+      { $push: { liked_by: username } },
+      { new: true }
+    );
+    // console.log(x, 137);
+    res.json(x);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+const dislikePost = async (req, res) => {
+  const { postId, username } = req.body;
+  try {
+    const x = await Post.findByIdAndUpdate(
+      { _id: postId },
+      { $pull: { liked_by: username } },
+      { new: true }
+    );
+    // res.json({ message: 'image disliked by ' + username })
+    res.json(x);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+// DELETE
+const deletePost = async (req, res) => {
+  let imgUrl = res.post.image;
+  const desertRef = fbStorage.ref(storage, imgUrl);
+  try {
+    await fbStorage.deleteObject(desertRef);
+    await res.post.deleteOne();
+    res.json({ message: "post deleted successfully" });
+  } catch (e) {
+    res.status(500).json({ message: { message: e.message } });
+  }
+};
+
+module.exports = {
+  allPosts,
+  searchPost,
+  singlePost,
+  downloadImage,
+  createPost,
+  updatePost,
+  likePost,
+  dislikePost,
+  deletePost,
+};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,193 +1,48 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer')
-const firebase = require("firebase/app");
-const fbStorage = require("firebase/storage");
+const router = require("express").Router();
+require("../config/firebase");
 
-const Post = require('../models/postModel');
-const { UserDetail } = require('../models/userModel')
-const requireAuth = require('../middleware/requireAuth')
+//middlewares
+const requireAuth = require("../middleware/requireAuth");
+const getPost = require("../middleware/getPost");
+const { upload } = require("../config/firebase");
 
+const {
+  allPosts,
+  updatePost,
+  searchPost,
+  likePost,
+  dislikePost,
+  deletePost,
+  createPost,
+  singlePost,
+  downloadImage,
+} = require("../controllers/postController");
 
-// GET all images
-router.get('/', async (req, res) => {
-    try {
-        const post = await Post.find({}).sort({ date: -1 });
-        res.json(post)
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
+// GET all posts
+router.get("/", allPosts);
 
-// Search images with given query
-router.get('/search/:query', async (req, res) => {
-    const query = req.params.query;
-    try {
-        // { email: new RegExp(`^${emailVariable}$`, 'i') }
-        const post = await Post.find({ tags: new RegExp(`^${query}$`, 'i') })
-        // console.log(post, 28);
-        res.json(post);
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
+// SEARCH posts with given query
+router.get("/search/:query", searchPost);
 
+//CREATE a new post
+router.post("/", requireAuth, upload.single("image"), createPost);
 
-//POST a new image
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
+// DOWNLOAD an image
+router.get("/download/:id", getPost, downloadImage);
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// GET a single post
+router.get("/photos/:id", getPost, singlePost);
 
-const storage = fbStorage.getStorage(app)
-const upload = multer({ storage: multer.memoryStorage() })
+// UPDATE a post
+router.patch("/:id", requireAuth, getPost, updatePost);
 
-router.post('/', requireAuth, upload.single('image'), async (req, res) => {
-    const { description, tags, location, username } = req.body;
-    const userDetails = await UserDetail.findOne({ username })
-
-    const tagsArr = String(tags).split(',');
-    tagsArr.forEach((ele, idx) => {
-        tagsArr[idx] = ele.trim();
-    });
-
-    try {
-        const storageRef = fbStorage.ref(storage, `files/${req.file.filename + " " + Date()}`)
-
-        const metadata = {
-            contentType: req.file.mimetype
-        }
-
-        // Upload the file in the bucket storage
-        const snapshot = await fbStorage.uploadBytesResumable(
-            storageRef,
-            req.file.buffer,
-            metadata
-        )
-        //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
-
-        // Grab the public url
-        const downloadURL = await fbStorage.getDownloadURL(snapshot.ref)
-
-        const newPost = new Post({
-            description: description,
-            tags: tagsArr,
-            location: location,
-            image: downloadURL,
-            user: userDetails,
-        })
-
-        const response = await newPost.save();
-
-        console.log("File successfully uploaded.")
-        res.status(201).json({
-            message: "file uploaded to firebase storage",
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            downloadURL: downloadURL
-        })
-    } catch (e) {
-        res.status(400).json({ message: e.message })
-    }
-})
-
-
-// middleware
-async function getPost(req, res, next) {
-    let post;
-    try {
-        post = await Post.findById(req.params.id)
-        if (post == null) {
-            return res.status(500).json({ message: 'could not find a post' })
-        }
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-
-    res.post = post
-    next()
-}
-
-router.get('/download/:id', getPost, async (req, res) => {
-    // console.log('96');
-    let imgPath = res.post.image;
-    res.download(`./public/${imgPath}`)
-})
-
-// GET a single image
-router.get('/photos/:id', getPost, async (req, res) => {
-    res.json(res.post);
-})
-
-// UPDATE an image
-router.patch('/:id', requireAuth, getPost, async (req, res) => {
-    const { description, tags, location } = req.body;
-
-    const tagsArr = String(tags).split(',');
-    tagsArr.forEach((ele, idx) => {
-        tagsArr[idx] = ele.trim();
-    });
-
-    if (res.post.description === description && res.post.tags === tagsArr && res.post.location === location) {
-        res.status(200).json({ message: 'post updated' })
-        return
-    }
-
-    res.post.description = description
-    res.post.tags = tagsArr
-    res.post.location = location
-
-    try {
-        const updatePost = await res.post.save()
-        res.status(200).json(updatePost)
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
-
-// DELETE an image
-router.delete('/:id', requireAuth, getPost, async (req, res) => {
-    let imgUrl = res.post.image;
-    const desertRef = fbStorage.ref(storage, imgUrl);
-    try {
-        await fbStorage.deleteObject(desertRef)
-        await res.post.deleteOne()
-        res.json({ message: 'post deleted successfully' })
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
+// DELETE a post
+router.delete("/:id", requireAuth, getPost, deletePost);
 
 // LIKE a post
-router.put('/like', requireAuth, async (req, res) => {
-    const { postId, username } = req.body;
-    try {
-        const x = await Post.findByIdAndUpdate({ _id: postId }, { $push: { liked_by: username } }, { new: true });
-        // console.log(x, 137);
-        res.json(x)
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
+router.put("/like", requireAuth, likePost);
 
-router.put('/unlike', requireAuth, async (req, res) => {
-    const { postId, username } = req.body;
-    try {
-        const x = await Post.findByIdAndUpdate({ _id: postId }, { $pull: { liked_by: username } }, { new: true });
-        // res.json({ message: 'image disliked by ' + username })
-        res.json(x);
-    } catch (e) {
-        res.status(500).json({ message: e.message })
-    }
-})
+// DISLIKE a post
+router.put("/unlike", requireAuth, dislikePost);
 
 module.exports = router;
